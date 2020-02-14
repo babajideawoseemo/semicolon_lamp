dbaf3b8b38462b5ed6445c6f136371127585e702
daunikman <daunikman@gmail.com>
2020-02-14 13:43:30 +0100

    APPLICANT CLASS ADDED

---
+++ lamp/lamp_data/src/main/java/com/lamp/Applicant.java
@@ -0,0 +1,16 @@
+package com.lamp;
+
+public class Applicant {
+
+	private String applicationId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String password;
+	private ApplicationStatus ApplicationStatus;
+	private  int AgeRange;
+	
+	
+	
+}
